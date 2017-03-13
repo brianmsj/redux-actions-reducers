c1cbdeb81661fc05e6194b528e06b3d05d1a1516
brianmsj <brianmsj@gmail.com>
2017-03-13 09:39:22 -0700

    added actions

---
+++ js/actions/index.js
@@ -0,0 +1,44 @@
+// randomly select a number between 1 and 100
+// player inputs a number
+// compare player selection to the random number
+// change state based upon how close player is to the number
+// increase guess count
+// log previous guesses
+
+// state = {
+//   randomNumber: 0,
+//   playerGuess: 0,
+//   guessCount: 0,
+//   guessHistory: [],
+//   currentStatus: "Make your Guess"
+//   whatPage: false
+//
+// }
+
+
+export const NEW_GAME = 'NEW_GAME';
+export const newGame = randomNumber => ({
+    type: NEW_GAME,
+    randomNumber
+});
+
+export const PLAYER_GUESS = 'PLAYER_GUESS';
+export const playerGuess = playerGuess => ({
+    type: PLAYER_GUESS,
+    playerGuess
+
+});
+
+export const WHAT = 'WHAT';
+export const what = () => ({
+    type: WHAT,
+    whatPage: true
+
+});
+
+export const GOT_IT = 'GOT_IT';
+export const gotIt = () => ({
+    type: PLAYER_GUESS,
+    whatPage: false
+
+});
